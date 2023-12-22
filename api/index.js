00e4fc4c9cdeb1dce5500f0e07aaba8a04a5acf9
prajwal wadhai <prajwalwadhai55@gmail.com>
2023-12-23 01:04:42 +0530

    create update user api route

--- api/index.js
+++ api/index.js
@@ -3,30 +3,37 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
     console.log('Connected to MongoDB!');
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
+
 const app = express();
 
 app.use(express.json());
 
-app.listen(3000 , () => {
-    console.log('Server is running on port 3000!');
-})
+app.use(cookieParser());
 
-app.use("/api/user", userRouter);  
-app.use("/api/auth", authRouter);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "Internal Server Error!";
-    return res.status(statusCode).json({
-        success: false,
-        statusCode,
-        message,
-    });
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
